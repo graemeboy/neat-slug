65f759878b1638675174d2bc6e0c5965fb6495a5
Graeme Boy <graemeboy@gmail.com>
2014-10-07 17:55:24 +0200

    Update directory structure

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-var filterText = require('./lib/textFilter').filterText;
+var textFilter = require('./lib/textFilter');
 
+
+/**
+ * A array of strings ordered by their popularity as hashtags
+ * on Twitter.
+ */
+var hashtagList;
+
+
+
+function getHashtagPopularity(str, callback) {
+
+}
+
+/**
+ * Order the list by the popularity of its items as hashtags.
+ * 
+ * @param  list, a list of words
+ * @return orderedList
+ */
+function orderListByPopularity(list) {
+
+}
 
 // For testing:
-exports.filterText = filterText;
+exports.textFilter = textFilter;

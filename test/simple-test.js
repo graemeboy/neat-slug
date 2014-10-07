6653c2e64654c0495af8ce1d6ee0a5e9b5267f8a
Graeme Boy <graemeboy@gmail.com>
2014-10-07 17:55:15 +0200

    Update test

--- test/simple-test.js
+++ test/simple-test.js
@@ -9,6 +9,6 @@ console.log("Running first test");
 //console.log(hashtags.filterText("123, 1, 1231243, ,324234 @!! 1E3Hello world!"));
 
 // Punctuation test:
-console.log(hashtags.filterText("This., -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctuation"));
+console.log(hashtags.textFilter("This., -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctuation"));
 // Numbers test
-console.log(hashtags.filterText("123 1 2 3 @This., -34/ i124s #! 1231a324n $ % ^ & 1* exa2123mple ;123: {} of123 a = 123123-_ string with `~)() punctuation"));
+console.log(hashtags.textFilter("123 1 2 3 @This., -34/ i124s #! 1231a324n $ % ^ & 1* exa2123mple ;123: {} of123 a = 123123-_ string with `~)() punctuation"));
